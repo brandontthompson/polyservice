ee2b66ea69160165dc1fc0bee534a7ed868ae205
Brandon Thompson <xhailorigin@gmail.com>
2021-10-22 09:13:04 -0700

    moved to MIT, removed unused packages

--- lib/imiddleware.ts
+++ lib/imiddleware.ts
@@ -3,6 +3,8 @@ import { IO } from "./iinterface";
 export interface imiddleware{
     interface: IO
     //@TODO: add support for multi namespaces
+    //@TODO: add support for pre-post buisness logic and both
+    // so we can run the middleware before the logic, and after the logc for logging or formating
     namespace?: string
     fnc: Function
 }
