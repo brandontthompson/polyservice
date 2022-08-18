339873f11d1a3c4b878f1da7710c3011d3ad730e
Brandon Thompson <xhailorigin@gmail.com>
2022-08-17 22:45:18 -0500

    Removed js files

--- controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.instanceOfController = void 0;
-function instanceOfController(object) {
-    return 'middleware' in object;
-}
-exports.instanceOfController = instanceOfController;
