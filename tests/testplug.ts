74b89278ee7e4930a3d3d354769c286726f4dc9a
Brandon Thompson <xhailorigin@gmail.com>
2022-08-18 02:41:48 -0500

    Removed dev tests

--- tests/testplug.ts
+++
@@ -1,9 +0,0 @@
-import { polyplugin } from "../polyplugin";
-import { polyservice } from "../polyservice";
-export const testplugin:polyplugin = {
-	execute(frame:polyservice){
-		frame.services().forEach((s:any, i:number) => {
-			s.id=1 << i;
-		})
-	}
-}
